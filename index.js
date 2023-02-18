4bd8313d5a173feb7f4b6a6d133905e866af169f
denis.ivanov <denis.ivanov@fabit.ru>
2023-02-18 18:49:53 +0200

    add 11th task

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const calculateStringBeuty = (inputString) => {
-  const [replacesCountString, stringToBeutify] = inputString.split('\n');
-  const replacesCount = Number(replacesCountString) + 1;
+class Stack {
+  elements = [];
 
-  // for (const [index, char] of stringToBeutify.split('').entries()) {
-  //   console.log(index, char);
-  // }
-  const repeatedChars = {};
-  for (let i = 0; i < stringToBeutify.length; i++) {
-    // if (stringToBeutify[i] === stringToBeutify[i + 1]){
-    //   repeatedChars[stringToBeutify[i]] = (repeatedChars[stringToBeutify[i]] || 0) + 1; 
-    // }
-    const current = repeatedChars[stringToBeutify[i]];
-    if (current) {
-      const { count, idx, maxBeauty } = current;
-      const isEqualToReplaceCount = i - idx[idx.length - 1] === replacesCount;
-      const isMoreThanReplaceCount = i - idx[idx.length - 1] > replacesCount;
-      const isLessThanReplaceCount = i - idx[idx.length - 1] < replacesCount;
-
-
-      let newIdx = [];
-      if (isEqualToReplaceCount) {
-        console.log(maxBeauty);
-        // console.log(stringToBeutify[i]);
-        newIdx = [idx[idx.length - 1], i]
-      } else if (isLessThanReplaceCount) {
-        // console.log(stringToBeutify[i]);
-        // newIdx = [idx[idx.length - 1], i]
-      }
-      else if (isMoreThanReplaceCount) {
-        // newIdx = [i];
-      }
-      repeatedChars[stringToBeutify[i]] = { count: count + 1, idx: newIdx }
-      // [...idx, i]
-    } else {
-      repeatedChars[stringToBeutify[i]] = {
-        count: (repeatedChars[stringToBeutify[i]] || 0) + 1, idx: [i], maxBeauty: replacesCount
-      }
-    }
+  push(n) {
+    this.elements.push(n);
+    return 'ok';
   }
-  console.log(repeatedChars);
-
-  return inputString;
+  pop() {
+    return this.elements.pop() || 'error';
+  }
+  size() {
+    return this.elements.length;
+  }
+  clear() {
+    this.elements = [];
+    return 'ok';
+  }
+  back() {
+    return this.elements[this.elements.length - 1] || 'error';
+  }
+  exit() {
+    return 'bye';
+  }
 }
 
+const logStack = (commands) => {
+  let stack = new Stack();
+  const log = commands.reduce((acc, command) => {
+    const [method, param] = command.split(' ');
+    if (method === 'exit') {
+      stack = null;
+      acc.push('bye');
+    }
+    if (stack) acc.push(stack[method](param));
+    return acc;
+  }, []);
+  return log.join('\n');
+}
 
 const fs = require('fs')
 const fileContent = fs.readFileSync("input.txt", "utf8");
-const result = calculateStringBeuty(fileContent.toString());
+const commands = fileContent.toString().trim().split('\n');
+const result = logStack(commands);
 
 fs.writeFileSync("output.txt", result);
