ee6dcdf4e945744907ff3ad976639b6aa0f89336
denis.ivanov <denis.ivanov@fabit.ru>
2023-02-23 20:41:24 +0200

    feat: add 18 task solution

---
+++ 18. Дек с защитой от ошибок/index.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+
+class Deque {
+  elements = [];
+
+  push_front(n) {
+    this.elements.unshift(n);
+    return 'ok';
+  }
+  push_back(n) {
+    this.elements.push(n);
+    return 'ok';
+  }
+  pop_front() {
+    return this.elements.shift() || 'error';
+  }
+  pop_back() {
+    return this.elements.pop() || 'error';
+  }
+  front() {
+    return this.elements[0] || 'error';
+  }
+  back() {
+    return this.elements[this.elements.length - 1] || 'error';
+  }
+  size() {
+    return this.elements.length;
+  }
+  clear() {
+    this.elements = [];
+    return 'ok';
+  }
+  exit() {
+    return 'bye';
+  }
+}
+
+const logStack = (commands) => {
+  let deque = new Deque();
+  const log = [];
+  for (const command of commands) {
+    const [method, param] = command.split(' ');
+    log.push(deque[method](param))
+    if (method === 'exit') {
+      break;
+    }
+  }
+
+  return log.join('\n');
+}
+
+
+const fileContent = fs.readFileSync("18. Дек с защитой от ошибок/input.txt", "utf8");
+const commands = fileContent.toString().trim().split('\n');
+const result = logStack(commands);
+
+fs.writeFileSync("output.txt", result);
